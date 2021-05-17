fff0e8b2a9da3c4059c1f5b65ef30c0bbf123755
Karen Roh <karenroh@hotmail.com>
2021-05-17 15:33:45 -0400

    created id, title, body, and user_id foreign key for Post model

---
+++ models/Post.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+Post.init(
+  //id, title, body, createdAt (later), updatedAt (later), userId (foreignKey)
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.STRING,
+      references: {
+        model: "User",
+        key: "id",
+      },
+    },
+  }
+);
