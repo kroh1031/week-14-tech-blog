ff63a4b62d56d06b967368d9d0665e3f424ca5b0
Karen Roh <karenroh@hotmail.com>
2021-05-17 15:14:37 -0400

    created id, username, and password fields for User model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class User extends Model {}
+
+User.init(
+  //id, username, password
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6],
+      },
+    },
+  }
+);
