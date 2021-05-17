08ff7c2ff80350e852b157b8f41306f6c4a67deb
Karen Roh <karenroh@hotmail.com>
2021-05-17 15:39:09 -0400

    created id, body, and post_id & user_id foreign keys for Comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,34 @@
 const { Model, DataTypes } = require("sequelize");
-const sequelize = require('../config/connection')
+const sequelize = require("../config/connection");
 
 class Comment extends Model {}
 
 Comment.init(
-    //
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        }
-    }
-
-
-)
+  //id, body, createdAt (later), updatedAt (later), postId, userId
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Post",
+        key: "id",
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "User",
+          key: "id",
+        },
+      },
+    },
+  }
+);
