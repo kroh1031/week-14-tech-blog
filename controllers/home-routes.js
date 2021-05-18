87c9f53825c58ab2c86c59d4a1d65c6ef6336ac0
Karen Roh <karenroh@hotmail.com>
2021-05-18 10:41:35 -0400

    login navlink directs user to login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
+
 router.get("/", (req, res) => {
   res.render("all-posts");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
 module.exports = router;
