605b0ae138f74ac8fb70021d6abb0689a2048550
Karen Roh <karenroh@hotmail.com>
2021-05-19 15:57:56 -0400

    trying to authenticate user in login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,23 @@
-const usernameEl = document.querySelector("#username-input-login");
-const passwordEl = document.querySelector("#password-input-login");
+const loginFormHandler = async (event) => {
+  event.preventDefault();
 
-console.log(usernameEl, passwordEl);
+  const username = document.querySelector("#username-input-login");
+  const password = document.querySelector("#password-input-login");
+
+  if (username && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to log in.");
+    }
+  }
+};
+
+document
+  .querySelector("#login-form")
+  .addEventListener("submit", loginFormHandler);
