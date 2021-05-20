382d611d79b8f9338207bcfda14b9d0ea9857482
Karen Roh <karenroh@hotmail.com>
2021-05-20 09:59:38 -0400

    redirects user to dashboard with all the user's posts if post successful

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  res.render("create-post", { layout: "dashboard" });
+  res.render("all-user-posts", { layout: "dashboard" });
 });
+
+router.get("/new", async (req, res) => {
+  res.render("create-post", { layout: "dashboard" });
+})
 module.exports = router;
