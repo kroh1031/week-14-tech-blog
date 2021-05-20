73c6b19eeeb4851ab936ed8e49e7e23e76da09bc
Karen Roh <karenroh@hotmail.com>
2021-05-20 10:32:30 -0400

    displaying newly created user posts on dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,10 +1,25 @@
 const router = require("express").Router();
+const { Post } = require("../models");
 
 router.get("/", async (req, res) => {
-  res.render("all-user-posts", { layout: "dashboard" });
+  try {
+    const dbPostData = await Post.findAll({
+      where: {
+        user_id: req.session.userId,
+      },
+    });
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    res.render("all-user-posts", {
+      posts,
+      loggedIn: req.session.loggedIn,
+      layout: "dashboard",
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get("/new", async (req, res) => {
   res.render("create-post", { layout: "dashboard" });
-})
+});
 module.exports = router;
