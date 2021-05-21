73a995ad9e8295e7e1d1915fe61359995a9403a6
Karen Roh <karenroh@hotmail.com>
2021-05-21 12:11:39 -0400

    successfully deletes post after creating route in  post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -27,4 +27,17 @@ router.put("/:id", withAuth, async (req, res) => {
   }
 });
 
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
