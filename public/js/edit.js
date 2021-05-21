3ec7d51c126970d4eb587f86205eb35f5d679259
Karen Roh <karenroh@hotmail.com>
2021-05-21 10:57:12 -0400

    created an editFormHandler function that executes the fetch function with a PUT method when Update Post button clicked on

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,1 +1,22 @@
-console.log("Successfully linked");
+const postId = document.querySelector("#post-id").value;
+
+const editFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("#title-input-post").value;
+  const body = document.querySelector("#content-input-post").value;
+
+  if (title && body) {
+    await fetch(`/api/posts/${postId}`, {
+      method: "PUT",
+      body: JSON.stringify({ title, body }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    document.location.replace("/dashboard");
+  }
+};
+
+document
+  .querySelector("#edit-form")
+  .addEventListener("submit", editFormHandler);
