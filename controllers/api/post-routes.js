4c21b42ac4088f346bc0e198154b2f9ed6405a88
Karen Roh <karenroh@hotmail.com>
2021-05-21 11:13:25 -0400

    added a put route to update the Post title and body in post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Post } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 router.post("/", async (req, res) => {
   try {
@@ -8,6 +9,19 @@ router.post("/", async (req, res) => {
       user_id: req.session.userId,
     });
     res.json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
